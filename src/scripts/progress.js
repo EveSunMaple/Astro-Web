ec44efe8af36c9be0e0194bf22675780154b6633
EveSunMaple <evesunmaple@outlook.com>
2024-03-02 21:02:05 +0800

    一些问题的修改

--- src/scripts/progress.js
+++ src/scripts/progress.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var winHeight = window.innerHeight,
-          docHeight = document.documentElement.scrollHeight,
-          progressBar = document.querySelector('#content_progress');
-    progressBar.max = docHeight - winHeight;
-    progressBar.value = window.scrollY;
+document.addEventListener('astro:page-load', () => {
+      document.addEventListener('DOMContentLoaded', function () {
+            var winHeight = window.innerHeight,
+                  docHeight = document.documentElement.scrollHeight,
+                  progressBar = document.querySelector('#content_progress');
+            progressBar.max = docHeight - winHeight;
+            progressBar.value = window.scrollY;
 
-    document.addEventListener('scroll', function () {
-          progressBar.max = document.documentElement.scrollHeight - window.innerHeight;
-          progressBar.value = window.scrollY;
-    });
+            document.addEventListener('scroll', function () {
+                  progressBar.max = document.documentElement.scrollHeight - window.innerHeight;
+                  progressBar.value = window.scrollY;
+            });
+      });
 });

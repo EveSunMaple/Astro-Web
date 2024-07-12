74ec11bcea71d009022b13097b8307ecd459776e
EveSunMaple <evesunmaple@outlook.com>
2024-07-12 18:24:48 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,26 +4,29 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import playformCompress from "@playform/compress";
+import pagefind from "astro-pagefind";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://www.saroprock.com',
-  integrations: [mdx(), sitemap(), tailwind()],
+  style: {
+    scss: {
+      includePaths: ['./src/styles']
+    }
+  },
+  integrations: [mdx(), sitemap(), tailwind(), playformCompress(), pagefind()],
   markdown: {
-    remarkPlugins: [
-      remarkMath,
-    ],
-    rehypePlugins: [
-      () => async (tree) => {
-        for (const node of tree.children) {
-          if (node.tagName === 'pre' && node.properties && node.properties.className) {
-            node.properties.className.push('mockup-code');
-          }
-          if (node.tagName === 'blockquote' && node.properties) {
-			node.properties.className = ['alert'];
-          }
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [() => async (tree) => {
+      for (const node of tree.children) {
+        if (node.tagName === 'pre' && node.properties && node.properties.className) {
+          node.properties.className.push('mockup-code');
         }
-      },
-      rehypeKatex
-    ]
-  }
+        if (node.tagName === 'blockquote' && node.properties) {
+          node.properties.className = ['alert'];
+        }
+      }
+    }, rehypeKatex]
+  },
 });

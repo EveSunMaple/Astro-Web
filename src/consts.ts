43dc5558e0e1a8cee31bb5c59d6523d5dc626a7a
EveSunMaple <evesunmaple@outlook.com>
2024-07-22 16:35:40 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,69 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const USER_NICKNAME = 'EveSunMaple';
-export const USER_DESCRIPTION = 'a student';
+// Site title and description
+export const SITE_TITLE = "SaroProck ⚡";
+export const SITE_DESCRIPTION = "一本本子";
+export const DATE_FORMAT = "ddd MMM DD YYYY";
+
+// User profile information
+export const USER_NAME = "EveSunMaple";
+export const USER_AVATAR = "/profile.webp";
+
+// Server and transition settings
+export const SERVER_URL = "https://waline.saroprock.com";
 export const TRANSITION_API = true;
+
+// Some informative text on the site
+export const infoTest = {
+  tag: "标签：",
+  noTag: "未分类",
+  tagPage: "博客 - ",
+  link: "链接：",
+  prevPage: "上一页",
+  nextPage: "下一页",
+};
+
+// Menu items for navigation
+export const menuItems = [
+  { id: "home", text: "首页", href: "/", svg: "home" }, // Home page
+  { id: "about", text: "关于", href: "/about", svg: "about" }, // About page
+  { id: "blog", text: "博客", href: "/blog", svg: "blog" }, // Blog page
+  { id: "project", text: "项目", href: "/project", svg: "project" }, // Projects page
+  { id: "friend", text: "朋友", href: "/friend", svg: "friend" }, // Friends page
+  {
+    id: "contact",
+    text: "联系",
+    href: "mailto:contact.evesunmaple@outlook.com", // Contact email
+    target: "_blank", // Open in a new tab
+    svg: "contact",
+  },
+];
+
+// Social media and contact icons
+export const socialIcons = [
+  {
+    href: "https://afdian.net/a/saroprock",
+    ariaLabel: "Support my work",
+    title: "Support my work",
+    svg: "support",
+  },
+  {
+    href: "https://github.com/EveSunMaple",
+    ariaLabel: "Github",
+    title: "Github",
+    svg: "github",
+  },
+  {
+    href: "https://space.bilibili.com/438392347",
+    ariaLabel: "BiliBili",
+    title: "BiliBili",
+    svg: "bilibili",
+  },
+  {
+    href: "/rss.xml",
+    ariaLabel: "RSS Feed",
+    title: "RSS Feed",
+    svg: "rss",
+  },
+];

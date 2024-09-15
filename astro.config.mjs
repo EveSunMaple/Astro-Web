ad487f4efc1c99ee67f053403320fe27b8bd6583
EveSunMaple <evesunmaple@outlook.com>
2024-09-15 22:23:42 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,6 +22,7 @@ import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.saroprock.com",
+  output: 'static',
   style: {
     scss: {
       includePaths: ["./src/styles"],

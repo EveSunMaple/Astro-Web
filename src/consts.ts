c0e405a7122cb986983344ffd753244ab37a0ba6
EveSunMaple <evesunmaple@outlook.com>
2024-12-05 01:04:49 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -10,7 +10,7 @@ export const DATE_FORMAT = "ddd MMM DD YYYY";
 
 // User profile information
 export const USER_NAME = "EveSunMaple";
-export const USER_SITE = "https://frosti.saroprock.com"
+export const USER_SITE = "https://www.saroprock.com"
 export const USER_AVATAR = "/profile.webp";
 
 // Server and transition settings
@@ -27,73 +27,52 @@ export const CODE_THEME = {
   dark: "github-dark",
 }
 
-// Some informative text on the site
-export const infoTest = {
-  tag: "标签：",
-  noTag: "未分类", //untagged
-  tagCard: "标签",
-  tagPage: "Tag - ",
-  noCategory: "未分类", //uncategorized
-  categoryCard: "分类",
-  categoryPage: "Category - ",
-  link: "链接：",
-  prevPage: "上一页",
-  nextPage: "下一页",
-};
-
 // Menu items for navigation
 export const menuItems = [
-  { id: "home", text: "首页", href: "/", svg: "home", target: "_self" }, // Home page
-  { id: "about", text: "关于", href: "/about", svg: "about", target: "_self" }, // About page
+  { id: "home", text: "首页", href: "/", svg: "material-symbols:home-outline-rounded", target: "_self" }, // Home page
+  { id: "about", text: "关于", href: "/about", svg: "material-symbols:info-outline-rounded", target: "_self" }, // About page
   {
     id: "blog",
     text: "博客",
     href: "/blog",
-    svg: "blog",
+    svg: "material-symbols:book-2-outline-rounded",
     target: "_self",
     subItems: [
       {
         id: "all",
         text: "所有博客",
         href: "/blog",
-        svg: "post",
+        svg: "material-symbols:ink-pen-outline-rounded",
         target: "_self",
       }, // All blog
       {
         id: "tech",
         text: "技术博客",
-        href: "/blog/categories/技术",
-        svg: "cube",
+        href: "/blog/categories/tech",
+        svg: "material-symbols:deployed-code-outline",
         target: "_self",
       }, // Technology category
       {
         id: "life",
         text: "生活博客",
-        href: "/blog/categories/生活",
-        svg: "heart",
+        href: "/blog/categories/life",
+        svg: "material-symbols:earthquake-rounded",
         target: "_self",
       }, // Life category
-      {
-        id: "categories",
-        text: "所有分类",
-        href: "/blog/categories",
-        svg: "categories",
-        target: "_self",
-      }, // All categories
     ],
   }, // Blog page with sub-items
   {
     id: "project",
     text: "项目",
     href: "/project",
-    svg: "project",
+    svg: "material-symbols:code-blocks-outline",
     target: "_self",
   }, // Projects page
   {
     id: "friend",
-    text: "朋友",
+    text: "友链",
     href: "/friend",
-    svg: "friend",
+    svg: "material-symbols:supervisor-account-outline-rounded",
     target: "_self",
   }, // Friends page
   {
@@ -101,7 +80,7 @@ export const menuItems = [
     text: "联系",
     href: "mailto:contact.evesunmaple@outlook.com", // Contact email
     target: "_blank", // Open in a new tab
-    svg: "contact",
+    svg: "material-symbols:attach-email-outline-rounded",
   },
 ];
 
@@ -111,31 +90,24 @@ export const socialIcons = [
     href: "https://afdian.net/a/saroprock",
     ariaLabel: "Support my work",
     title: "Support my work",
-    svg: "support",
+    svg: "ri:cup-line",
   },
   {
     href: "https://github.com/EveSunMaple",
     ariaLabel: "Github",
     title: "Github",
-    svg: "github",
+    svg: "ri:github-line",
   },
   {
     href: "https://space.bilibili.com/438392347",
     ariaLabel: "BiliBili",
     title: "BiliBili",
-    svg: "bilibili",
+    svg: "ri:bilibili-line",
   },
   {
     href: "/rss.xml",
     ariaLabel: "RSS Feed",
     title: "RSS Feed",
-    svg: "rss",
+    svg: "ri:rss-line",
   },
-];
-// Category Information
-export const categoriesInfo = [
-  { title: "技术", desc: "有关技术的一些文章", target: "_self" },
-  { title: "生活", desc: "生活中的所见所思所想", target: "_self" },
-  { title: "OI学习笔记", desc: "OIer 的“圣遗物”", target: "_self" },
-  { title: "作文", desc: "和随笔有些不同", target: "_self" },
 ];
--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -2,13 +2,14 @@ import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
-export async function GET(context) {
+export async function GET(context: any) {
   const posts = await getCollection("blog");
+  const sortedPosts = posts.sort((a: any, b: any) => new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime());
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
+    items: sortedPosts.map((post: any) => ({
       ...post.data,
       link: `/blog/${post.slug}/`,
     })),

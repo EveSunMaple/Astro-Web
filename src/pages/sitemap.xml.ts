bf3bd9465cce1fdc4894b9a7d04ce28f13fb710a
EveSunMaple <evesunmaple@outlook.com>
2025-02-21 00:09:25 +0800

    feat: add sitemap and robots.txt endpoints, enhance post reactions component

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,16 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export async function GET(context: any) {
+  const blogs = await getCollection("blog");
+  const sortedblogs = blogs.sort((a: any, b: any) => new Date(b.data.pubDate).getTime() - new Date(a.data.pubDate).getTime());
+  return rss({
+    title: "SaroProck",
+    description:  "Personal blog about technology, programming, and life.",
+    site: context.site,
+    items: sortedblogs.map((blog: any) => ({
+      ...blog.data,
+      link: `/blog/${blog.slug}/`,
+    })),
+  });
+}
